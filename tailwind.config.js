f601189892494a44fc88502b9a94e1e56788b5ad
nishida namiki <ymdnmk1201@gmail.com>
2026-05-17 14:22:24 +0900

    feat: カスタムテーマとユーティリティを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "laravel-red": "#F05340",
+        "vue-green": "#42b883",
+        "custom-blue": "#1E40AF",
+        "custom-green": {
+          light: "#6EE7B7",
+          DEFAULT: "#10B981",
+          dark: "#047857",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        mono: ["Fira Code", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".writing-vertical-rl": {
+          "writing-mode": "vertical-rl",
+        },
+      });
+    }),
+  ],
 };
